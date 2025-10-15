65f54da9e3c514cd3970306f13d93045ee440845
wangfangyong <wangfangyong@itiger.com>
2025-10-15 21:48:02 +0800

    MOD:update cronjob

--- avatar/cronjob/cronjob.go
+++ avatar/cronjob/cronjob.go
@@ -58,11 +58,13 @@ func Stop() {
 	}
 }
 
-// ExampleTasks 添加一些示例任务,后续添加任务，只需要在这个方法里加AddTask就行了
+// AddTasks 添加一些示例任务,后续添加任务，只需要在这个方法里加AddTask就行了
 func AddTasks() {
-	AddTask("*/10 * * * * *", func() {
-		hlog.Infof("任务1 每10秒执行一次 %v", time.Now())
+	// 每30秒执行一次
+	AddTask("*/30 * * * * *", func() {
+		hlog.Infof("任务1 每30秒执行一次 %v", time.Now())
 	})
 
-	AddTask("*/5 * * * * *", service.CronTaskFunc)
+	// 每分钟执行一次，service包中的func可以作为cron来执行
+	AddTask("0 * * * * *", service.CronTaskFunc)
 }
